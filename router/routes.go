0a3eb0189a3345726dcae21374b8632de420abe0
Joao <joaodslourenco@gmail.com>
2024-02-29 15:12:47 -0300

    feat: create crud routes

--- router/routes.go
+++ router/routes.go
@@ -2,14 +2,49 @@ package router
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+func generateEndpoint(method, endpoint string) string {
+	return fmt.Sprintf("%s %s", method, endpoint)
+}
+
 func initializeRoutes(router *http.ServeMux) {
-	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+
+	router.HandleFunc(generateEndpoint(http.MethodGet, "/opening"), func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", jsonContentType)
 		json.NewEncoder(w).Encode(JSONTest{
-			Message: "pong",
+			Message: "GET opening",
 		})
 	})
+
+	router.HandleFunc(generateEndpoint(http.MethodPost, "/opening"), func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonContentType)
+		json.NewEncoder(w).Encode(JSONTest{
+			Message: "POST opening",
+		})
+	})
+
+	router.HandleFunc(generateEndpoint(http.MethodDelete, "/opening"), func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonContentType)
+		json.NewEncoder(w).Encode(JSONTest{
+			Message: "DELETE opening",
+		})
+	})
+
+	router.HandleFunc(generateEndpoint(http.MethodPut, "/opening"), func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonContentType)
+		json.NewEncoder(w).Encode(JSONTest{
+			Message: "PUT opening",
+		})
+	})
+
+	router.HandleFunc(generateEndpoint(http.MethodGet, "/openings"), func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonContentType)
+		json.NewEncoder(w).Encode(JSONTest{
+			Message: "GET openings",
+		})
+	})
+
 }
