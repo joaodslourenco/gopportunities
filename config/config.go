c1535a344dfdb2558b83ac733e156dc9e31d4d31
Joao <joaodslourenco@gmail.com>
2024-02-29 16:50:10 -0300

    chore: implementation of db

--- config/config.go
+++ config/config.go
@@ -1,17 +1,34 @@
 package config
 
 import (
-	"errors"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 var (
 	logger *Logger
 )
 
-func Init() error {
-	// db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname))
+var (
+	host     string = "localhost"
+	port     int    = 5432
+	user     string = "postgres"
+	password string = "password" // Replace with your actual password
+	dbname   string = "mydatabase"
+)
 
-	return errors.New("fake error")
+func Init() error {
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname))
+
+	if err != nil {
+		logger.Errorf("could not start Postgres: %v", err)
+	}
+
+	logger.Info(db)
+
+	return nil
 }
 
 func GetLogger(p string) *Logger {
---
+++ schemas/opening.go
@@ -0,0 +1,10 @@
+package schemas
+
+type Opening struct {
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
